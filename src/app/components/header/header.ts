a623e39afbea4a7b11e6631ef3b38da499459372
radzio <radek.kozera1@gmail.com>
2026-09-17 12:04:14 +0200

    feat: basic navigation

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   styleUrl: './header.scss',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  private router = inject(Router);
+
+  protected navigateToTable(): void {
+    this.router.navigate(['/']);
+  }
+
+  protected navigateToOrder(): void {
+    this.router.navigate(['new-order']);
+  }
+}
