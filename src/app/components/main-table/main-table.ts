be767bb0217b46911eda4c0f0e21e05a0b25f857
radzio <radek.kozera1@gmail.com>
2026-09-17 12:12:32 +0200

    feat: display row group data in main-table

--- src/app/components/main-table/main-table.ts
+++ src/app/components/main-table/main-table.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+
+import { StateService } from '../../services/state/state.service';
+import { OrderGroupRow } from '../order-group-row/order-group-row';
 
 @Component({
+  imports: [OrderGroupRow],
   selector: 'app-main-table',
   styleUrl: './main-table.scss',
   templateUrl: './main-table.html',
 })
-export class MainTable {}
+export class MainTable {
+  private readonly state = inject(StateService);
+
+  protected readonly groups = this.state.groups;
+}
--- src/app/components/order-group-row/order-group-row.ts
+++ src/app/components/order-group-row/order-group-row.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+import { OrderGroup } from '../../services/state/state.domain';
 
 @Component({
+  host: { role: 'row' },
   selector: 'app-order-group-row',
   styleUrl: './order-group-row.scss',
   templateUrl: './order-group-row.html',
 })
-export class OrderGroupRow {}
+export class OrderGroupRow {
+  public readonly group = input.required<OrderGroup>();
+}
